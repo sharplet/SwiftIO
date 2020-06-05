5eac5b14c47b89327e81401e5ce23544a5f1ddfb
Adam Sharp <adam@sharplet.me>
2020-06-05 15:35:12 -0400

    Export POSIXError and POSIXErrorCode

--- Sources/SwiftIO/POSIXErrorExtensions.swift
+++ Sources/SwiftIO/POSIXErrorExtensions.swift
@@ -1,4 +1,6 @@
-import Foundation
+@_exported import struct Foundation.POSIXError
+@_exported import enum Foundation.POSIXErrorCode
+import var Foundation.errno
 
 extension POSIXError {
   public static var errno: POSIXError {
